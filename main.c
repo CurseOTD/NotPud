4fe790be88127ce8764ad08d6ebe7e7601f46a5d
CurseOTD <max-cun4@yandex.ru>
2018-10-27 22:04:51 +0300

    Changed window settings

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main(int argc, char* argv[]) {
 	gtk_init(&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+	gtk_window_set_title(GTK_WINDOW(window), "NotPud");
 
 	gtk_widget_show(window);
 	
